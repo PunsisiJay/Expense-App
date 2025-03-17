f1ae831e885ff1486ed763f79e886c73378ce452
Punsisi Jayasundera <punsisijayasundera11@gmail.com>
2025-03-17 12:48:21 +0530

    initial Project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Expenz());
+}
+
+class Expenz extends StatelessWidget {
+  const Expenz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
